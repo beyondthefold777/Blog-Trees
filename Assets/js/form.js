db1c8bbcfa476ce15836b4ae6cf4092fcf944576
anthony bell <anthonybell67.ab@gmail.com>
2024-03-24 22:57:40 -0400

    created const variables for form.js file along click event & page redirect

---
+++ Assets/js/form.js
@@ -0,0 +1,8 @@
+const userNameInput = document.querySelector('#user-name');
+const blogTitleInput = document.querySelector('#blog-title');
+const blogContentInput = document.querySelector('#blog-content');
+const submitButton = document.querySelector('#submit');
+
+submitButton.addEventListener('click', function () {
+    window.location.href="Blog-trees.html";
+  }
