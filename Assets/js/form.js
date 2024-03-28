ae380a08ea4165af3910195f7ec5cc3c4de00390
anthony bell <anthonybell67.ab@gmail.com>
2024-03-28 07:42:20 -0400

    added the click to the submit button by putting a formaction url within the button tags on my index file

--- Assets/js/form.js
+++ Assets/js/form.js
@@ -1,8 +1,81 @@
-const userNameInput = document.querySelector('#user-name');
-const blogTitleInput = document.querySelector('#blog-title');
-const blogContentInput = document.querySelector('#blog-content');
-const submitButton = document.querySelector('#submit');
+const form = document.getElementById('form');
+const userName = document.getElementById('userName');
+const blogTitle = document.getElementById('blogTitle');
+const blogContent = document.getElementById('blogContent');
+const mybtn = document.getElementById('submit')
 
-submitButton.addEventListener('click', function () {
-    window.location.href="Blog-trees.html";
-  }
+document.addEventListener(`DOMContentLoaded`, () => {
+document.getElementById('form').onsubmit = 
+function (event) {
+event.preventDefault()
+console.log('submit')
+
+});
+
+const userNameInput = userName.value.trim();
+const blogTitleInput = blogTitle.value.trim();
+const blogContentInput = blogContent.value.trim();
+
+if (!userNameInput || !blogTitleInput || !blogContentInput) {
+    alert("Please submit the form");
+    console.log('invalid input') 
+    return
+}
+
+
+const user = {
+    userName: userNameInput,
+    blogTitle: blogTitleInput,
+    blogContent: blogContentInput,
+  };
+
+
+
+// local storage object
+
+localStorage.setItem('user', JSON.stringify(user));
+
+
+
+
+
+
+
+
+
+
+
+
+// submission to local storage
+
+
+
+
+     
+  
+
+ 
+   
+
+  
+
+
+  
+    
+ 
+    
+//     //      let messages = []
+//     //   if (name.value === '' || name.value == null)
+//     //   messages.push ('Form Submission Is Required')
+    
+    
+    // if (messages.length > 0) {
+        //     e.preventDefault()
+        //     errorElement.innerText = messages.join(',')
+        // }
+        
+        
+        
+    
+    
+    
