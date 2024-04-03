9d947921536f341a9eee1b9e4b3d54026dca2759
anthony bell <anthonybell67.ab@gmail.com>
2024-04-02 23:45:11 -0400

    working on a statement to alert the user if the form is empty

---
+++ Assets/js/blog.js
@@ -0,0 +1,5 @@
+
+
+localStorage.getItem("blogData")
+
+
--- Assets/js/form.js
+++ Assets/js/form.js
@@ -3,37 +3,52 @@ const userName = document.getElementById('userName');
 const blogTitle = document.getElementById('blogTitle');
 const blogContent = document.getElementById('blogContent');
 const mybtn = document.getElementById('submit')
-
+//Submit button function
 document.addEventListener(`DOMContentLoaded`, () => {
-document.getElementById('form').onsubmit = 
-function (event) {
-event.preventDefault()
-console.log('submit')
+    document.getElementById('form').onsubmit = 
+    function (event) {
+        event.preventDefault()
+        console.log('submit')
+        
+    }});
+    
+    const userNameInput = userName.value.trim();
+    const blogTitleInput = blogTitle.value.trim();
+    const blogContentInput = blogContent.value.trim();
+    
+    //if statement for empty form input
+    // if (userNameInput ==null || blogTitleInput ==null || blogContentInput ==null) {
+    //     alert("Please submit the form");
+    //     console.log('invalid input') 
+    //     return false;
+    // }
+    
+    let messages = []
+    if (name.value === '' || name.value == null)
+    messages.push ('Form Submission Is Required')
+    
+    
+    if (messages.length > 0) {
+       e.preventDefault()
+       errorElement.innerText = messages.join(',')
+    }
+    
+    //creation of array for our objects
+    let blogData = [] 
+//object
+    const user = {
+    userName: userName,
+    blogTitle: blogTitle,
+    blogContent: blogContent,
+    }
+    blogData.push(user)
+    console.log(blogData)
 
-});
 
-const userNameInput = userName.value.trim();
-const blogTitleInput = blogTitle.value.trim();
-const blogContentInput = blogContent.value.trim();
-
-if (!userNameInput || !blogTitleInput || !blogContentInput) {
-    alert("Please submit the form");
-    console.log('invalid input') 
-    return
-}
+// local storage object array
+localStorage.setItem('blogData', JSON.stringify(blogData));
 
 
-const user = {
-    userName: userNameInput,
-    blogTitle: blogTitleInput,
-    blogContent: blogContentInput,
-  };
-
-
-
-// local storage object
-
-localStorage.setItem('user', JSON.stringify(user));
 
 
 
@@ -64,15 +79,6 @@ localStorage.setItem('user', JSON.stringify(user));
     
  
     
-//     //      let messages = []
-//     //   if (name.value === '' || name.value == null)
-//     //   messages.push ('Form Submission Is Required')
-    
-    
-    // if (messages.length > 0) {
-        //     e.preventDefault()
-        //     errorElement.innerText = messages.join(',')
-        // }
         
         
         
