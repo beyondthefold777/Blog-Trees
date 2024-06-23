4e72e1bdb686d3a3a1264cc3cf94d453562e142a
beyondthefold777 <beyondthefold1989@gmail.com>
2024-06-23 10:28:20 -0400

    finished updates

--- Assets/js/logic.js
+++ Assets/js/logic.js
@@ -1,12 +1,14 @@
 const toggle = document.getElementById('toggle');
 const toggleDot = document.querySelector('.toggle-dot');
 
-toggle.addEventListener('change', function () {
-    if (toggle.checked) {
-        toggleDot.style.transform = 'translateX(100%)';
-        toggleDot.style.backgroundColor = '#f6e05e'; // Change color for the dot when toggled
-    } else {
-        toggleDot.style.transform = 'translateX(0)';
-        toggleDot.style.backgroundColor = 'gray'; // Reset color for the dot when untoggled
-    }
+toggle.addEventListener('change', function() {
+  if (toggle.checked) {
+    document.body.classList.add('dark-mode');
+    toggleDot.style.transform = 'translateX(100%)';
+    toggleDot.style.backgroundColor = '#f6e05e';
+  } else {
+    document.body.classList.remove('dark-mode');
+    toggleDot.style.transform = 'translateX(0)';
+    toggleDot.style.backgroundColor = '#cccccc';
+  }
 });
